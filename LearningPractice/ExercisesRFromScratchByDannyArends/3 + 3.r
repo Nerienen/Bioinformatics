ec87dc67dfc749efe1ddf2035f155cb368ab6c4a
[Nerien] Yevgeniy Gugala Fomin <nerienart@gmail.com>
2024-08-02 10:56:27 +0800

    Exercise 1 Update: R as a calculator

---
+++ LearningPractice/ExercisesRFromScratchByDannyArends/3 + 3.r
@@ -0,0 +1,3 @@
+
+print((2 / 2) + (3 + 3 ))
+print(" potato")
---
+++ LearningPractice/ExercisesRFromScratchByDannyArends/Assignments1/answers.R
@@ -0,0 +1,44 @@
+# Analysis of Hardy-Weinberg equilibrium
+#
+# copyright (c) 2024 - Yevgeniy Gugala Fomin
+# written by: Yevgeniy Gugala Fomin
+# last modified 1 Aug, 2024
+# first written 1 Aug, 2024
+
+# Instruct R to go to the working directory
+setwd("C:\\Users\\user\\Documents\\GitHub\\Bioinformatics\\LearningPractice\\ExercisesRFromScratchByDannyArends\\Assignments1")
+
+# R as a calculator
+
+# Use R to calculate the following:
+# 1a) 1234 + 4567
+
+    print(paste("1a =", 1234 + 4567))
+
+# 1b) 100456 - 3350 + 23
+
+    print(paste("1b =", 1234 + 4567))
+
+# 1c) natural logarithm of 15
+
+    print(paste("1c =", log(15)))
+
+# 1d) 4596 / 12
+
+    print(paste("1d =", 4596 / 12))
+
+# 1e) 8998 * 76
+
+    print(paste("1e =", 8998 * 76))
+
+# 1f) Euclidean division remainder of 10 and 6 (When you divide 10 / 6 , what number remains?)
+
+    print(paste("1f", 10 %% 6))
+
+# 1g) the square root of -8 (Square root = what number multiplied by itself will give you said number)
+
+    print(paste("1g =", sqrt_neg8 <- sqrt(-8 + 0i)))
+    
+
+
+
